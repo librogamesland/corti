e88ee442e01bd889f78c93b836612d3d1fcafe22
lucafabbian <luc.fabbian@gmail.com>
2021-01-09 23:40:51 +0100

    Sito 0.2 (work in progress)

--- sito/src/index.js
+++ sito/src/index.js
@@ -1,10 +1,40 @@
 console.log("hellonnmm")
-
+import elencoFile from '../elenco-file.json'
 import {ultimeNews, archivio} from '../ultime-news.js'
+
+
+console.log(elencoFile['2021'].corti)
+
+const baseUrl="https://librogamesland.github.io/corti/"
+
+const testoArchivio = Object.keys(elencoFile).sort().reverse().map( (key,i) =>
+  `<section id="section${key}">
+  <input type="radio" name="sections" id="option${key}" ${i == 0 ? 'checked':''}>
+  <label for="option${key}">${key}</label>
+  <article>
+    <h2>Edizione ${key}</h2>
+    <table style="margin-bottom: 20px">
+    ${!(elencoFile[key].files.includes('libro.pdf')) ? '' :
+    `<tr><td><a href="${baseUrl + key + '/libro.pdf'}" target="_blank" download>Libro dei corti ${key}</a></td></tr>`  
+    }
+    
+    ${!(elencoFile[key].files.includes('bando.pdf')) ? '' :
+    `<tr><td><a href="${baseUrl + key + '/bando.pdf'}" target="_blank" download>Bando edizione ${key}</a></td></tr>`  
+    }
+    </table>
+    ${!('corti' in elencoFile[key]) ? '' : `<table>${
+      elencoFile[key].corti.map(corto => 
+        `<tr><td><a href="${baseUrl + key + '/corti/' + corto}" target="_blank" download>${corto}</a></td></tr>`  
+      ).join('\n')
+    } </table>`}
+  </article>
+</section>`
+).join('\n')
 
 
 window.addEventListener("load", function(event) {
     console.log("Tutte le risorse hanno terminato il caricamento!");
     document.getElementById('ultime-news').innerHTML = ultimeNews
+    document.getElementById('tab-archivio').innerHTML = testoArchivio
 
   });
