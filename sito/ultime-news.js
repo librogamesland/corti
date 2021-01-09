616a02a33ecf63575d2bcb76e5952d3ef1567d57
lucafabbian <luc.fabbian@gmail.com>
2021-01-09 12:03:54 +0100

    Aggiunto sito test1

---
+++ sito/ultime-news.js
@@ -0,0 +1,11 @@
+const ultimeNews = `
+<h1>Chi siamo?</h1>
+Il concorso dei corti è un concorso bello.<br>
+Qui il bando del concorso: <a href="bit.ly/corti2021">bando.pdf</a>
+`
+
+const archivio = `
+L'archivio dei corti
+`
+
+export {ultimeNews, archivio}
