d13d29a45ac4f3d7039ed8c1050315f7b079ab15
lucafabbian <luc.fabbian@gmail.com>
2021-01-10 14:55:22 +0100

    Sito 0.4 (work in progress)

--- sito/ultime-news.js
+++ sito/ultime-news.js
@@ -1,13 +1,13 @@
 const ultimeNews = `
 <h1>Chi siamo?</h1><hr>
-<p>"I corti di <a href="http://librogame.net" target="_blank">Librogame's Land</a>" 
-&egrave; il più grande concorso italiano dedicato ai racconti interattivi.</p>
+<p>"I corti di <a href="http://librogame.net" target="_blank">Librogame's Land</a>" è il
+più grande concorso italiano dedicato ai racconti interattivi.</p>
 
 <p>Gestito dalla community con il supporto delle case editrici del settore, il concorso consente agli autori
 di sfidarsi su un tema che cambia di anno in anno, sottoponendo i propri racconti brevi e brevissimi al giudizio
 del pubblico.</p>
 
-<p>Attualmente &egrave in corso la <b>13sima edizione</b> del concorso. Gli autori hanno tempo fino 
+<p>Attualmente è in corso la <b>13sima edizione</b> del concorso. Gli autori hanno tempo fino 
 al <b>31/01/2021</b> per consegnare le proprie opere.</p>
 <p>Bando con le istruzioni su come partecipare:
 <a href="https://bit.ly/corti2021" target="_blank" download>bando.pdf</a></p>
@@ -17,4 +17,6 @@ const archivio = `
 L'archivio dei corti
 `
 
-export {ultimeNews, archivio}
+const footer = `<p>@ 2021 Librogame's Land</p>`
+
+export {ultimeNews, archivio, footer}
