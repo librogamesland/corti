a56d9ce9adcb0dee100f6ab7b425074605b08852
Luca Fabbian <luca.fabbian.1999@gmail.com>
2021-02-22 00:34:45 +0100

    Aggiunte soluzioni

--- sito/src/elenco.js
+++ sito/src/elenco.js
@@ -21,6 +21,45 @@ fs.readdirSync(rootFolder).filter(anno => anno.startsWith('2') ).forEach(anno =>
 });
 
 
+const votazioni = {
+  "01-[Dedalo-Re:loaded].pdf": "https://www.facebook.com/cortiLGL/posts/117281443664436",
+  "02-Arriverai-qui.pdf": "https://www.facebook.com/cortiLGL/posts/117281743664406",
+  "03-BlueScreens.pdf": "https://www.facebook.com/cortiLGL/posts/117281910331056",
+  "04-Demoniache-presenze.pdf" : "https://www.facebook.com/cortiLGL/posts/117282053664375",
+  "05-DESLAGRATE-La-magia esiste.pdf" : "https://www.facebook.com/cortiLGL/posts/117282176997696",
+  "06-Dopo-cena.pdf" : "https://www.facebook.com/cortiLGL/posts/117282290331018",
+  "07-Eidolon.pdf" : "https://www.facebook.com/cortiLGL/posts/117283340330913",
+  "08-Escape-Labirinto-Infernale.pdf" : "https://www.facebook.com/cortiLGL/posts/117283603664220",
+  "09-Figlio-di-Due-Piani.pdf" : "https://www.facebook.com/cortiLGL/posts/117283776997536",
+  "10-Filastrocca-game.pdf" : "https://www.facebook.com/cortiLGL/posts/117283860330861",
+  "11-Fuoco-Alieno.pdf" : "https://www.facebook.com/cortiLGL/posts/117283970330850",
+  "12-Fuori-Concorso.pdf" : "https://www.facebook.com/cortiLGL/posts/117284040330843",
+  "13-Goblin.pdf" : "https://www.facebook.com/cortiLGL/posts/117284116997502",
+  "14-Guerre-e-distruzione.pdf" : "https://www.facebook.com/cortiLGL/posts/117284233664157",
+  "15-Il-Precetto-Finale.pdf" : "https://www.facebook.com/cortiLGL/posts/117284420330805",
+  "16-Il-varco-oscuro.pdf" : "https://www.facebook.com/cortiLGL/posts/117284653664115",
+  "17-InOut.pdf" : "https://www.facebook.com/cortiLGL/posts/117284710330776",
+  "18-La-deviazione.pdf" : "https://www.facebook.com/cortiLGL/posts/117284770330770",
+  "19-Lasciami-andare.pdf" : "https://www.facebook.com/cortiLGL/posts/117284923664088",
+  "20-Le-ombre-del-mio-gatto.pdf" : "https://www.facebook.com/cortiLGL/posts/117285000330747",
+  "21-L'Eroe-dei-Due-Mondi.pdf" : "https://www.facebook.com/cortiLGL/posts/117285133664067",
+  "22-Morte-agli-spaghetti.pdf" : "https://www.facebook.com/cortiLGL/posts/117285223664058",
+  "23-Nel-nome-dell'imperatore.pdf" : "https://www.facebook.com/cortiLGL/posts/117285400330707",
+  "24-Ocram.pdf" : "https://www.facebook.com/cortiLGL/posts/117285510330696",
+  "25-Operazione-Valkyrja.pdf" : "https://www.facebook.com/cortiLGL/posts/117285563664024",
+  "26-P-616.pdf" : "https://www.facebook.com/cortiLGL/posts/117285710330676",
+  "27-Superfire-Pro-Fighter-Premium-Version.pdf" : "https://www.facebook.com/cortiLGL/posts/117285836997330",
+  "28-Viaggio-nell'Immanenza.pdf" : "https://www.facebook.com/cortiLGL/posts/117286000330647",
+  "29-Vinland-Saga-2.pdf" : "https://www.facebook.com/cortiLGL/posts/117286090330638",
+}
+
+
+const voteButton = (edizione, corto) => { 
+  if(edizione != '2021') return ""
+  if(! (corto in votazioni)) return ""
+  return `<a href="${votazioni[corto]}" class="button vote" rel="noopener" target="_blank">Vota<img src="sito/res/link.svg" alt="(link esterno)"height="12px" style="margin-left: 7px;"></a>`
+}
+
 
 const archivio = 
 '<select aria-label="Seleziona edizione" class="only-mobile" onchange="document.getElementById(this.value).checked = true">' +
@@ -48,8 +87,8 @@ Object.keys(elenco).sort().reverse().map( (key,i) =>
     }
     </table>
     ${!('corti' in elenco[key]) ? '' : `<table>${
-      elenco[key].corti.map(corto => 
-        `<tr><td><a href="${baseUrl + key + '/corti/' + corto}"  rel="noopener" target="_blank">${corto}</a></td></tr>`  
+      elenco[key].corti.sort( (a,b) => a.replace(".pdf","").localeCompare(b.replace(".pdf", "")) ).map(corto => 
+        `<tr><td><a href="${baseUrl + key + '/corti/' + corto}"  rel="noopener" target="_blank">${corto}</a>${voteButton(key, corto)}</td></tr>`  
       ).join('\n')
     } </table>`}
   </article>
---
+++ sito/src/votazioni.js
@@ -0,0 +1,12 @@
+const anno = "2021"
+const corti = {
+    "01323": "02 - Arriverai qui"
+} 
+
+
+const votazioni = `<div id="votazioni" class="page">
+<h1>Votazioni ${anno} </h1>
+<div class="posts-votazioni">
+</div>
+</div>`
+export {votazioni}
