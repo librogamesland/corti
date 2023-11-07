ebe9aedae4d552c79b3f8e9a322f48e90fe08bf3
Luca Fabbian <luca.fabbian.1999@gmail.com>
2023-11-07 15:04:24 +0100

    Nuovo sito senza build

--- res/sito/votazioni.js
+++ res/sito/votazioni.js
@@ -1,26 +1,3 @@
-import fs from 'fs'
-import path from 'path'
-
-
-const baseUrl="https://librogamesland.github.io/corti/"
-
-
-// Elenca le edizioni
-const elenco = {}
-const rootFolder = path.dirname(path.dirname(__dirname))
-// Filtra le cartelle, prendi solo quelle che iniziano con 2
-fs.readdirSync(rootFolder).filter(anno => anno.startsWith('2') ).forEach(anno => {
-    // Elenca i corti se la cartella esiste
-    let corti = []
-    try { corti = fs.readdirSync(path.join(rootFolder, anno, 'corti'))
-    }catch(e){}
-    
-    // Fa un elenco delle altre risorse
-    const files = fs.readdirSync(path.join(rootFolder, anno)).filter(item => item != 'corti')
-    elenco[anno] = {corti, files}
-});
-
-
 const votazioni = {
   "01-[Dedalo-Re:loaded].pdf": "https://www.facebook.com/cortiLGL/posts/117281443664436",
   "02-Arriverai-qui.pdf": "https://www.facebook.com/cortiLGL/posts/117281743664406",
@@ -52,47 +29,3 @@ const votazioni = {
   "28-Viaggio-nell'Immanenza.pdf" : "https://www.facebook.com/cortiLGL/posts/117286000330647",
   "29-Vinland-Saga-2.pdf" : "https://www.facebook.com/cortiLGL/posts/117286090330638",
 }
-
-
-const voteButton = (edizione, corto) => { 
-  if(! (corto in votazioni)) return ""
-  return `<a href="${votazioni[corto]}" class="button vote" rel="noopener" target="_blank">Vota<img src="sito/res/link.svg" alt="(link esterno)"height="12px" style="margin-left: 7px;"></a>`
-}
-
-
-const archivio = 
-'<select aria-label="Seleziona edizione" class="only-mobile" onchange="document.getElementById(this.value).checked = true">' +
-Object.keys(elenco).sort().reverse().map( (key, i) => 
-  `<option value="option${key}">Edizione ${key}</option>`
-).join('/n') + '</select>' +
-
-'<div class="labels only-desktop">' +
-Object.keys(elenco).sort().reverse().map( (key,i) => 
-  `<label for="option${key}">${key}</label>`
-).join('\n') + '</div><div class="sections">'
-
-+ Object.keys(elenco).sort().reverse().map( (key,i) =>
-  `<section id="section${key}">
-  <input type="radio" name="sections" id="option${key}" ${i == 0 ? 'checked':''}>
-  <article>
-    <h2 class="only-desktop">Edizione ${key}</h2>
-    <table style="margin-bottom: 20px">
-    ${!(elenco[key].files.includes('libro.pdf')) ? '' :
-    `<tr><td><a href="${baseUrl + key + '/libro.pdf'}"  rel="noopener" target="_blank">Libro dei corti ${key}</a></td></tr>`  
-    }
-
-    ${!(elenco[key].files.includes('bando.pdf')) ? '' :
-    `<tr><td><a href="${baseUrl + key + '/bando.pdf'}"  rel="noopener" target="_blank">Bando edizione ${key}</a></td></tr>`  
-    }
-    </table>
-    ${!('corti' in elenco[key]) ? '' : `<table>${
-      elenco[key].corti.sort( (a,b) => a.replace(".pdf","").localeCompare(b.replace(".pdf", "")) ).map(corto => 
-        `<tr><td><a href="${baseUrl + key + '/corti/' + corto}"  rel="noopener" target="_blank">${corto}</a>${voteButton(key, corto)}</td></tr>`  
-      ).join('\n')
-    } </table>`}
-  </article>
-</section>`
-).join('\n') + '</div>'
-
-  
-export {elenco, archivio}
